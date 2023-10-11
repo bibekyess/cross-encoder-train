25db154c0f58f6528294cecebe195b411356a853
Bibek K C <bibekyess@gmail.com>
2023-10-11 18:59:32 +0900

    Updates with mlflow logging part

--- kfp/src/model_utilities.py
+++ kfp/src/model_utilities.py
@@ -2,6 +2,13 @@ from torch.utils.data import DataLoader
 from sentence_transformers.cross_encoder import CrossEncoder
 from sentence_transformers.cross_encoder.evaluation import CERerankingEvaluator
 
+
+from typing import Dict, Any, Optional
+import numpy as np
+import mlflow
+import pandas as pd
+import torch
+
 #We set num_labels=1, which predicts a continous score between 0 and 1
 def get_model(model_name, num_labels, max_length):
     return CrossEncoder(model_name=model_name, num_labels=num_labels, max_length=max_length)
@@ -14,3 +21,30 @@ def get_train_dataloader(dataset, batch_size, shuffle=True):
 # It performs a classification task and measures scores like F1 (finding relevant passages) and Average Precision
 def get_evaluator(samples, name='train-eval', mrr_at_k=10):
     return CERerankingEvaluator(samples=samples, name=name, mrr_at_k=mrr_at_k)
+
+class CrossEncoderWrapper(mlflow.pyfunc.PythonModel):
+    def load_context(self, context: Dict[str, Any]) -> bool:
+        # Load SentenceTransformer from disk.
+        self.model = torch.load(context.artifacts["model_path"])
+        self.ready = True
+        return self.ready
+    
+    def predict(self, context: Dict[str, Any], model_input: pd.DataFrame,  params: Optional[Dict[str, Any]] = None) -> np.ndarray:
+        
+        print(model_input)
+
+        inputs = model_input.get("inputs")
+        # InferenceRequest is not automatically decoded
+        if inputs:
+            model_input_ = {}
+            for inp in inputs:
+                model_input_[inp['name']] = inp['data']
+            model_input = model_input_
+
+        query = model_input["query"]
+        paragraphs = model_input["paragraphs"]
+        input = []
+        for p in paragraphs:
+            input.append([query[0], p])  
+        scores = self.model.predict(input)
+        return scores
--- kfp/src/training_pipeline_kfp.py
+++ kfp/src/training_pipeline_kfp.py
@@ -14,6 +14,15 @@ from kfp import dsl
 from kfp.client import Client
 from kfp.dsl import Input, Output, Model, Dataset
 
+import numpy as np
+import mlflow
+import sentence_transformers
+import cloudpickle
+from sys import version_info
+
+from kubernetes import client, config
+import base64
+
 seed = 777
 # np.random.seed(seed)
 random.seed(seed)
@@ -28,14 +37,14 @@ logging.basicConfig(format='%(asctime)s - %(message)s',
 
 timestr = datetime.now().strftime("%y%m%d_%H%M")
 
-# @dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13')
+# @dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27')
 # def load_data_op(datasets_root: str, dataset_name: str, p_mask: float) -> NamedTuple(
 #                 'outputs', train_corpus=Dict[str, str], train_queries=Dict[str, str], train_triples= List[List[str]]):
 #     train_corpus, train_queries, train_triples = du.load_data(datasets_root, dataset_name, p_mask)
 #     outputs = NamedTuple('outputs', train_corpus=Dict[str, str], train_queries=Dict[str, str], train_triples= List[List[str]])
 #     return outputs(train_corpus, train_queries, train_triples)
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13')
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27')
 def load_data_op(datasets_root: str, dataset_name: str, p_mask: float,
                  train_corpus: Output[Dataset], train_queries: Output[Dataset], train_triples: Output[Dataset]) -> None:
     train_corpus_value, train_queries_value, train_triples_value = du.load_data(datasets_root, dataset_name, p_mask)
@@ -51,7 +60,7 @@ def load_data_op(datasets_root: str, dataset_name: str, p_mask: float,
 
 
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13')
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27')
 def create_train_dev_triples_op(train_triples_all: Input[Dataset], train_triples: Output[Dataset], dev_triples: Output[Dataset]) -> None:
     
     with open(train_triples_all.path, "rb") as pickle_file:
@@ -66,7 +75,7 @@ def create_train_dev_triples_op(train_triples_all: Input[Dataset], train_triples
         pickle.dump(dev_triples_value, pickle_file)  
 
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13')
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27')
 def create_dev_data_op(dev_triples: Input[Dataset], 
                        train_queries: Input[Dataset], 
                        train_corpus: Input[Dataset], 
@@ -88,8 +97,8 @@ def create_dev_data_op(dev_triples: Input[Dataset],
         pickle.dump(dev_samples_value, pickle_file) 
 
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13',
-               packages_to_install=['sentence-transformers==2.2.2', 'tqdm==4.66.1'])
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
+               packages_to_install=['sentence-transformers', 'tqdm'])
 def create_train_data_op(dev_samples: Input[Dataset],
                          train_triples: Input[Dataset], 
                          train_queries: Input[Dataset], 
@@ -115,32 +124,34 @@ def create_train_data_op(dev_samples: Input[Dataset],
     with open(train_samples.path, "wb") as pickle_file:
         pickle.dump(train_samples_value, pickle_file) 
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13',
-               packages_to_install=['sentence-transformers==2.2.2'])
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
+               packages_to_install=['sentence-transformers'])
 def get_model_op(load_model_name: str, num_labels: int, max_length: int, model: Output[Model]) -> None:
     cross_encoder_model = mu.get_model(model_name=load_model_name, num_labels=num_labels, max_length=max_length)
     torch.save(cross_encoder_model, model.path)
 
 
-# @dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13',
+# @dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
 #                packages_to_install=['torch', 'sentence-transformers', 'tqdm', 'numpy'])
 # def get_train_dataloader(train_samples: Dict[str, Any], train_batch_size: int):
 #     train_dataloader = mu.get_train_dataloader(dataset=train_samples, batch_size=train_batch_size)
 #     return train_dataloader
 
 
-# @dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13',
+# @dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
 #                packages_to_install=['torch', 'sentence-transformers', 'tqdm', 'numpy']):
 # def get_evaluator(dev_samples):
 #     evaluator = mu.get_evaluator(samples=dev_samples)
 #     return evaluator
 
-@dsl.component(base_image='python:3.11-slim', target_image='bibekyess/cross-encoder-train:v13',
-               packages_to_install=['torch==2.1.0', 'sentence-transformers==2.2.2'])
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
+               packages_to_install=['sentence-transformers', 'boto3'],
+)
 def train_model_op(model: Input[Model], num_epochs: int, evaluation_steps: int, warmup_steps: int,
                 learning_rate: float,
                 train_samples: Input[Dataset], train_batch_size: int,
-                dev_samples: Input[Dataset]) -> None:
+                dev_samples: Input[Dataset],
+                output_model: Output[Model]) -> None:
     with open(dev_samples.path, "rb") as pickle_file:
         dev_samples_value = pickle.load(pickle_file)
 
@@ -153,8 +164,6 @@ def train_model_op(model: Input[Model], num_epochs: int, evaluation_steps: int, 
     
     cross_encoder_model = torch.load(model.path)
 
-    # Train the model
-    # FIX ME Commenting this training snippet is working!!
     cross_encoder_model.fit(train_dataloader=train_dataloader,
             evaluator=evaluator,
             epochs=num_epochs,
@@ -162,7 +171,46 @@ def train_model_op(model: Input[Model], num_epochs: int, evaluation_steps: int, 
             warmup_steps=warmup_steps,
             optimizer_params={'lr': learning_rate},
             use_amp=True)
+    
+    torch.save(cross_encoder_model, output_model.path)
 
+
+@dsl.component(base_image='nvidia/cuda:11.8.0-base-ubuntu20.04', target_image='bibekyess/cross-encoder-train:v27',
+               packages_to_install=['sentence-transformers', 'mlflow', 'cloudpickle', 'boto3'],
+)
+def log_model_mlflow_registry_op(model: Input[Model]) -> None:
+
+    # mlflow must be running with database backend
+    mlflow.set_tracking_uri('http://192.168.0.29:5000') # FIXME
+    PYTHON_VERSION = f"{version_info.major}.{version_info.minor}.{version_info.micro}"
+    conda_env = {
+        "channels": ["defaults"],
+        "dependencies": [
+            f"python={PYTHON_VERSION}",
+            "pip",
+            {
+                "pip": [
+                    f"mlflow=={mlflow.__version__}",
+                    f"sentence-transformers=={sentence_transformers.__version__}",
+                    f"cloudpickle=={cloudpickle.__version__}",
+                    f"numpy=={np.__version__}"
+                ],
+            },
+        ],
+        "name": "cross_encoder_env",
+    }
+    cross_encoder_model = torch.load(model.path)
+    model_path = './saved_model.pth'
+    torch.save(cross_encoder_model, model_path)
+    
+    # Save the MLflow Model
+    mlflow_pyfunc_model_path = "cross_encoder_pyfunc"
+    mlflow.pyfunc.log_model(
+        artifact_path = mlflow_pyfunc_model_path,
+        python_model=mu.CrossEncoderWrapper(),
+        artifacts={"model_path": model_path},
+        conda_env=conda_env
+    )
 
 @dsl.pipeline
 def my_pipeline() -> None:
@@ -213,6 +261,18 @@ def my_pipeline() -> None:
                                     train_batch_size=train_batch_size,
                                     dev_samples= create_dev_data_task.output
                                     )
+    log_model_mlflow_registry_task = log_model_mlflow_registry_op(model=train_model_task.output)
+
+    envs = ["MLFLOW_S3_ENDPOINT_URL", "AWS_ACCESS_KEY_ID" , "AWS_SECRET_ACCESS_KEY" , "MLFLOW_S3_IGNORE_TLS"]
+    # set_env_variable(name: str, value: str)
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+    kube_secret = v1.read_namespaced_secret("mlflow-secret", "kubeflow-user-example-com").data
+    for name in envs:
+        log_model_mlflow_registry_task.set_env_variable(
+                name=name,
+                value= base64.b64decode(kube_secret.get(name)).decode('utf-8')
+                )
 
 def start_training_pipeline_run():
     import requests
@@ -220,7 +280,7 @@ def start_training_pipeline_run():
     USERNAME = "user@example.com"
     PASSWORD = "12341234" 
     NAMESPACE = "kubeflow-user-example-com"
-    HOST = "http://localhost:8084" # your istio-ingressgateway pod ip:8080
+    HOST = "http://192.168.0.33:8084" # your istio-ingressgateway pod ip:8080
 
     session = requests.Session()
     response = session.get(HOST)
